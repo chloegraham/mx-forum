05d4be1fa2ef569df47729e77722a0e9fbbf1568
Chloe <chloe.graham10@gmail.com>
2017-02-24 00:02:26 +1300

    created api for show user

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,13 +1,36 @@
 class API::SessionsController < ApplicationController#Devise::SessionsController
-  skip_before_filter :verify_authenticity_token,
-                     :if => Proc.new { |c| c.request.format == 'application/json' }
+  #skip_before_filter :verify_authenticity_token,
+   #                  :if => Proc.new { |c| c.request.format == 'application/json' }
 
-  respond_to :json
+  #respond_to :json
 
   # login
   # accepts a user JSON object as POST data with an email and a password parameters and returns an auth_token if the user exists in the database and the password is correct
+  
+  #before_create :generate_authentication_token
+
+  #ef generate_authentication_token
+    #loop do
+      #self.authentication_token = SecureRandom.base64(64)
+      #break unless User.find_by(authentication_token: authentication_token)
+    #end
+  #end
+
+
   def create
-    warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+    # => @var = JSON.parse(params[:name_of_the_JSON_fields])
+
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+
+
+    #warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged in",
